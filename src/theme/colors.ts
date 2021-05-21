bdf58b97f5e4df4b3b03f6ac4cc90015c419aa38
CrashBanditCoin14 <crashbanditcoin@gmail.com>
2021-05-21 23:26:59 +0200

    update color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -23,9 +23,9 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#008E46",
+  text: "#191326",
   textDisabled: "#BDC2C4",
-  textSubtle: "#008E46",
+  textSubtle: "#191326",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
