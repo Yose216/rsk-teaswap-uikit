cfee352c335003201b67fe868ba35101f4e6a966
CrashBanditCoin14 <crashbanditcoin@gmail.com>
2021-06-04 22:57:31 +0200

    update

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2843,7 +2843,7 @@ var baseColors = {
     primaryBright: "#008E46",
     primaryDark: "#008E46",
     secondary: "#008E46",
-    success: "#31D0AA",
+    success: "#008E46",
     warning: "#FFB237",
 };
 var brandColors = {
@@ -2852,7 +2852,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#008E46", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#524B63", textDisabled: "#BDC2C4", textSubtle: "#008E46", borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#008E46", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#D40B53", primaryDark: "D40B53", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#008E46", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#C170FD", primaryDark: "D40B53", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
