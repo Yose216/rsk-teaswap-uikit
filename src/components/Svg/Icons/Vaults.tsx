83ab6f14caaa462aaf34592fbdd1045041349148
CrashBanditCoin14 <crashbanditcoin@gmail.com>
2021-06-21 03:52:38 +0200

    add vault icon

--- src/components/Svg/Icons/Vaults.tsx
+++ src/components/Svg/Icons/Vaults.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Svg from "../../../components/Svg/Svg";
+import Svg from "../Svg";
 import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
