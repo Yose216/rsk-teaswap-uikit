33cdd4242b773362d435917565316241939f6361
Developer <85023510+carlatic-dev@users.noreply.github.com>
2021-06-22 10:56:21 -0700

    Update repository

--- dist/widgets/Menu/types.d.ts
+++ dist/widgets/Menu/types.d.ts
@@ -1,3 +1,4 @@
+import { Colors } from "../../theme/types";
 import { Login } from "../WalletModal/types";
 export interface LangType {
     code: string;
@@ -18,10 +19,15 @@ export interface NavTheme {
     background: string;
     hover: string;
 }
+export interface LinkStatus {
+    text: string;
+    color: keyof Colors;
+}
 export interface MenuSubEntry {
     label: string;
     href: string;
     calloutClass?: string;
+    status?: LinkStatus;
 }
 export interface MenuEntry {
     label: string;
@@ -30,18 +36,16 @@ export interface MenuEntry {
     href?: string;
     calloutClass?: string;
     initialOpenState?: boolean;
+    status?: LinkStatus;
 }
 export interface PanelProps {
     isDark: boolean;
     toggleTheme: (isDark: boolean) => void;
     cakePriceUsd?: number;
-    mintPriceUsd?: number;
-    teasportPriceUsd?: number;
     currentLang: string;
     langs: LangType[];
     setLang: (lang: LangType) => void;
     links: Array<MenuEntry>;
-    priceLink: string;
 }
 export interface NavProps extends PanelProps {
     account?: string;
--- src/widgets/Menu/types.ts
+++ src/widgets/Menu/types.ts
@@ -1,3 +1,4 @@
+import { Colors } from "../../theme/types";
 import { Login } from "../WalletModal/types";
 
 export interface LangType {
@@ -23,10 +24,16 @@ export interface NavTheme {
   hover: string;
 }
 
+export interface LinkStatus {
+  text: string;
+  color: keyof Colors;
+}
+
 export interface MenuSubEntry {
   label: string;
   href: string;
   calloutClass?: string;
+  status?: LinkStatus;
 }
 
 export interface MenuEntry {
@@ -36,19 +43,17 @@ export interface MenuEntry {
   href?: string;
   calloutClass?: string;
   initialOpenState?: boolean;
+  status?: LinkStatus;
 }
 
 export interface PanelProps {
   isDark: boolean;
   toggleTheme: (isDark: boolean) => void;
   cakePriceUsd?: number;
-  mintPriceUsd?: number;
-  teasportPriceUsd?: number;
   currentLang: string;
   langs: LangType[];
   setLang: (lang: LangType) => void;
   links: Array<MenuEntry>;
-  priceLink: string;
 }
 
 export interface NavProps extends PanelProps {
