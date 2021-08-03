f0e5e1a1d8682453531e2430b938816edf01d0b0
CrashBandiCoin <77974644+CrashBandiCoin@users.noreply.github.com>
2021-08-03 02:29:28 +0200

    update width of logo

--- src/widgets/Menu/icons/Sugar.tsx
+++ src/widgets/Menu/icons/Sugar.tsx
@@ -4,7 +4,7 @@ import {Svg, SvgProps} from "../../../components/Svg";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 90 90" {...props}>
-      <image width={90} height={110} href="/images/egg/9.png"/>
+      <image width={70} height={90} href="/images/egg/9.png"/>
     </Svg>
   );
 };
