04c6f8adb7f78027deb6b2c4a60714ad8bbc80ff
CrashBandiCoin <88516022+adamChafqani@users.noreply.github.com>
2021-09-14 02:53:17 +0200

    update logo, label, text

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -3392,7 +3392,7 @@ var Icon$w = function (props) {
 var Logo$1 = function (_a) {
     _a.isDark; var props = __rest(_a, ["isDark"]);
     return (React__default['default'].createElement(Svg, __assign({ viewBox: "0 0 160 26" }, props),
-        React__default['default'].createElement("image", { width: "305", height: "30", href: "/images/egg/LogoTextNewWhite.svg" })));
+        React__default['default'].createElement("image", { width: "205", height: "26", href: "/images/egg/LogoTextNewWhite.png" })));
 };
 
 var Icon$v = function (props) {
